5b1323cb5140553e2ac688793e2bb1cafdc9d725
Andreas Ek <andreas@andreasek.se>
2016-01-11 23:32:07 +0100

    No composer on local docker container

--- bin/tests.sh
+++ bin/tests.sh
@@ -5,7 +5,7 @@ export WP_DEVELOP_DIR="/usr/src/testrunner/wordpress-develop"
 cd /usr/src/testrunner/wordpress-develop/src/wp-content/plugins
 mkdir -p theplugin
 cd theplugin
-rsync -r --exclude={vendor,.git} /usr/src/plugin/. .
-rm -Rf vendor
-composer update
+rsync -rE --exclude={vendor,.git} /usr/src/plugin/. .
+#rm -Rf vendor
+#composer update
 vendor/bin/phpunit > testresult.txt
